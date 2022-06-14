7700f1ef46fbe7c5cf96967302a3d1b19e53b338
Kesney Mendes <kesneymendes@gmail.com>
2022-06-13 22:38:09 -0300

    type group and swap

--- src/Components/BoxesGroup.tsx
+++ src/Components/BoxesGroup.tsx
@@ -3,25 +3,26 @@ import Box from "./Box";
 
 export function BoxesGroup() {
 
-	const arrInventory: { id?: number, name?: number }[] = [
+	type group = { id?: number, name?: number }[]
+	const arrInventory: group = [
 		{ id: 1, name: 110 }, { id: 2, name: 115 }, { id: 3, name: 364 }, { id: 4, name: 243 }, { id: 5, name: 225 }, { id: 6, name: 4 }, { id: 7 }, { id: 8 }, { id: 9 },
 		{ id: 10 }, { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }, { id: 17 }, { id: 18 },
 		{ id: 19 }, { id: 20 }, { id: 21 }, { id: 22 }, { id: 23 }, { id: 24 }, { id: 25 }, { id: 26 }, { id: 27 },
 	]
 
-    const arrSelect: { id?: number, name?: number }[] = [
+    const arrSelect: group = [
 		{ id: 28 }, { id: 29 }, { id: 30 }, { id: 31 }, { id: 32 }, { id: 33 }, { id: 34 }, { id: 35 }, { id: 36 }
 	]
 
-	const arrArmory: { id?: number, name?: number }[] = [
+	const arrArmory: group = [
 		 { id: 37}, { id: 38 }, { id: 39 }, { id: 40 }
 	]
 
-	const arrCraft:  { id?: number, name?: number }[] = [
+	const arrCraft:  group = [
 		{ id: 41 }, { id: 42 }, { id: 43 }, { id: 44 }
    ]
 
-   const arrCrafted:  { id?: number, name?: number }[] = [
+   const arrCrafted:  group = [
 	{ id: 45 }
 ]
 
@@ -31,7 +32,10 @@ export function BoxesGroup() {
 	const [isDragging, setIsDragging] = useState<boolean>(false)
 
 	const swapBoxes = (fromBox: any, toBox: any) => {
-		let boxeSlice = inventoryBoxes.slice();
+    
+
+		let boxeSlice = inventoryBoxes
+
 		let fromIndex = -1;
 		let toIndex = -1;
 
@@ -43,7 +47,6 @@ export function BoxesGroup() {
 				toIndex = i;
 			}
 		}
-
 
 		if (fromIndex !== -1 && toIndex !== -1) {
 			let { fromId, ...fromRest }: { fromId?: number } & { name?: number } = boxeSlice[fromIndex];
@@ -94,15 +97,14 @@ export function BoxesGroup() {
 		setIsDragging(false);
 		let fromBox = JSON.parse(event.dataTransfer.getData("dragContent"));
 		let toBox = { id: data.id };
-		swapBoxes(fromBox, toBox);
+
+
+		swapBoxes(fromBox, toBox)
 		return false;
 	};
 
 	
-	const makeBoxes = (prop:{
-		id?: number | undefined;
-		name?: number | undefined;
-	}[]) => {
+	const makeBoxes = (prop:group) => {
 		
 
 
