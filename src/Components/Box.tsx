76f6dd8ae53076a79d8110be5ac9da9079ed59d2
Kesney Mendes <kesneymendes@gmail.com>
2022-06-18 12:54:17 -0300

    Result not droppable

--- src/Components/Box.tsx
+++ src/Components/Box.tsx
@@ -4,12 +4,16 @@ type Props = {
 	box: {
 		id?: number | undefined;
 		name?: number | undefined;
-		area?: string | undefined;
+		class?: string | undefined;
 	},
 	draggable: boolean,
 	onDragStart: (arg0: { id: any; }) => DragEventHandler<HTMLDivElement> | undefined,
 	onDragOver: (arg0: { id: any; }) => DragEventHandler<HTMLDivElement> | undefined,
-	onDrop: (arg0: { id: any; }) => DragEventHandler<HTMLDivElement> | undefined,
+	onDrop: (arg0: {
+		id?: number | undefined;
+		name?: number | undefined;
+		class?: string | undefined;
+	}) => DragEventHandler<HTMLDivElement> | undefined,
 	onEnter: () => DragEventHandler<HTMLDivElement> | undefined,
 
 	isDragging: boolean
@@ -17,14 +21,15 @@ type Props = {
 
 export default function Box({ box, draggable, onDragStart, onDragOver, onDrop, isDragging, onEnter }: Props) {
 
-
+//se box.class.includes("armory") onDrop({id:box.id})
+// se não tem armory, não droppable
 	return (
 		<div
-			className={box.area?`box ${box.area}`:"box"}
+			className={box.class?`box ${box.class}`:"box"}
 			onDragEnter={onEnter}
 			onDragStart={onDragStart({ id: box.id })}
 			onDragOver={onDragOver({ id: box.id })}
-			onDrop={onDrop({ id: box.id })}
+			onDrop={onDrop(box)}
 		>
 			{box.name && <img className="content" draggable src={`http://www.101computing.net/mc/${box.name}-0.png`} alt={`${box.name}`} />}
 		</div>
--- src/Components/BoxesGroup.tsx
+++ src/Components/BoxesGroup.tsx
@@ -3,14 +3,14 @@ import Box from "./Box";
 
 export function BoxesGroup() {
 
-	type group = { id?: number, name?: number, area?:string }[]
+	type group = { id?: number, name?: number, class?:string }[]
 	const arrInventory: group = [
-		{ id: 42, area:"crafting-1" },{ id: 43, area:"crafting-2" },{ id: 44, area:"crafting-3" },{ id: 45, area:"crafting-4" },{ id: 46, area:"result" },
-		{ id: 37, area:"armory helmet"}, { id: 38, area:"armory chest" }, { id: 39, area:"armory legging"}, { id: 40, area:"armory boot" },{ id: 41, area:"armory shield" },
+		{ id: 42, class:"crafting-1" },{ id: 43, class:"crafting-2" },{ id: 44, class:"crafting-3" },{ id: 45, class:"crafting-4" },{ id: 46, class:"result" },
+		{ id: 37, class:"armory helmet"}, { id: 38, class:"armory chest" }, { id: 39, class:"armory legging"}, { id: 40, class:"armory boot" },{ id: 41, class:"armory shield" },
 		{ id: 1, name: 110 }, { id: 2, name: 115 }, { id: 3, name: 364 }, { id: 4, name: 243 }, { id: 5, name: 225 }, { id: 6, name: 4 }, { id: 7 }, { id: 8 }, { id: 9 },
 		{ id: 10}, { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }, { id: 17 }, { id: 18 },
 		{ id: 19 }, { id: 20 }, { id: 21 }, { id: 22 }, { id: 23 }, { id: 24 }, { id: 25 }, { id: 26 }, { id: 27 },
-		{ id: 28, area:"select-group", name :118 }, { id: 29, area:"select-group" }, { id: 30, area:"select-group" }, { id: 31,area:"select-group" }, { id: 32, area:"select-group" }, { id: 33, area:"select-group" }, { id: 34, area:"select-group" }, { id: 35, area:"select-group" }, { id: 36, area:"select-group delete" }
+		{ id: 28, class:"select-group", name :118 }, { id: 29, class:"select-group" }, { id: 30, class:"select-group" }, { id: 31,class:"select-group" }, { id: 32, class:"select-group" }, { id: 33, class:"select-group" }, { id: 34, class:"select-group" }, { id: 35, class:"select-group" }, { id: 36, class:"select-group delete" }
 		
 	]
 
@@ -41,14 +41,14 @@ export function BoxesGroup() {
 
 
 		//se os index foram atribuidos
-		//pega o name e area no index
+		//pega o name e class no index
 		//Troca os elementos de inventory box
 		
 		if (fromIndex !== -1 && toIndex !== -1) {
-			let { fromId, ...fromRest }: { fromId?: number } & { name?: number, area?:string } = inventoryBoxes[fromIndex];
-			let { toId, ...toRest }: { toId?: number } & { name?: number, area?:string } = inventoryBoxes[toIndex];
-			inventoryBoxes[fromIndex] = { id: fromBox.id, name:toRest.name, area:fromRest.area };
-			inventoryBoxes[toIndex] = { id: toBox.id, name:fromRest.name, area:toRest.area };
+			let { fromId, ...fromRest }: { fromId?: number } & { name?: number, class?:string } = inventoryBoxes[fromIndex];
+			let { toId, ...toRest }: { toId?: number } & { name?: number, class?:string } = inventoryBoxes[toIndex];
+			inventoryBoxes[fromIndex] = { id: fromBox.id, name:toRest.name, class:fromRest.class };
+			inventoryBoxes[toIndex] = { id: toBox.id, name:fromRest.name, class:toRest.class };
 
 		}
 		
@@ -84,15 +84,22 @@ export function BoxesGroup() {
 
 	};
 
-	const handleDrop = (data: { id: any; }) => (event: {
+	const handleDrop = ( box: {
+		id?: number | undefined;
+		name?: number | undefined;
+		class?: string | undefined;
+	}) => (event: {
 		preventDefault: () => void; dataTransfer: {
 			getData: (arg0: string) => string;
 		};
 	}) => {
 		event.preventDefault();
 		setIsDragging(false);
+
+		if(box.class?.includes("result")){return}
+
 		let fromBox = JSON.parse(event.dataTransfer.getData("dragContent"));
-		let toBox = { id: data.id };
+		let toBox = { id: box.id };
 
 
 		swapBoxes(fromBox, toBox)
